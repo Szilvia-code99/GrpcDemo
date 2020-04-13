0dac70fa43d8e6ea4ef25915fb82ad7e38a59151
Szilvia <szilviakovacs55@gmail.com>
2020-04-14 00:27:03 +0300

    Commit 2: Read in name of client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -9,14 +9,24 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            //var input = new HelloRequest { Name = "Tim" };
+            Console.WriteLine("Please type your name: ");
+            var input = new HelloRequest { Name = Console.ReadLine() };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
 
             //var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            //var client = new Greeter.GreeterClient(channel);
+            //var customerClient = new Customer.CustomerClient(channel);
 
-            //var reply = await client.SayHelloAsync(input);
+            //var clientRequested = new CustomerLookUpModel { UserId = 3 } ;
 
-            //Console.WriteLine(reply.Message);
+            //var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            //Console.WriteLine($"{customer.FirstName}{customer.LastName}");
 
             Console.ReadLine();
         }
